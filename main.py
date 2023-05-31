ee7122873f6318e6451526191bb905e2fa784b9d
Johnnpham7496 <johnpham7496@gmail.com>
2023-05-30 22:29:47 -0500

    add dbsetup endpoint and route to main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Depends
+from db import user_db
+from db_config import engine
+from repository import user_repository
+from sqlalchemy.orm import Session
+from db_config import get_db
+
+
 
 app = FastAPI(
     title="Johnny's-FastAPI",
@@ -18,3 +25,13 @@ async def welcome(response: Response):
 async def heatlh(response: Response):
     response.status_code = status.HTTP_200_OK
     return{"status": "OK"}
+
+
+@app.get("/dbsetup")
+async def create_db(db: Session = Depends(get_db)):
+    user_db.Base.metadata.drop_all(engine)
+    user_db.Base.metadata.create_all(engine)
+    user_repository.add_user_td(db)
+    response_text = '{"message": "Database created."}'
+    response = Response(content=response_text, status_code=200, media_type='application/json')
+    return response
--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm.session import Session
-from db.user_db import UserDb
+from db.user_db import Userdb
 
 
 def add_user(db: Session, _username, _email, _role):
-    new_user = UserDb(username=_username, email=_email, role=_role)
+    new_user = Userdb(username=_username, email=_email, role=_role)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
